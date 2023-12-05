5175868b63942f0cb49a44a7ec422db1dd8f2451
lucky790 <64120130+lucky790@users.noreply.github.com>
2023-12-05 18:29:17 +0530

    Update app.js

--- app.js
+++ app.js
@@ -41,6 +41,7 @@ const activeHoursEnd = 20; // 10:00pm
 io.on('connection', (socket) => {
     console.log("a user connected", socket.id);
  const currentHour = new Date().getHours();
+    console.log(currentHour);
 
      // Check if the current time is within the active hours
      if (currentHour < activeHoursStart || currentHour > activeHoursEnd) {
